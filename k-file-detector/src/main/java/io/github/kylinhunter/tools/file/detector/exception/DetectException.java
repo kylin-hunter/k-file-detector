80d750eabd736b49ac8d529608c4dec89b32a49e
kylinhunter <250917500@qq.com>
2022-11-24 22:01:12 +0800

    init

--- k-file-detector/src/main/java/io/github/kylinhunter/tools/file/detector/exception/DetectException.java
+++ k-file-detector/src/main/java/io/github/kylinhunter/tools/file/detector/exception/DetectException.java
@@ -1,11 +1,13 @@
 package io.github.kylinhunter.tools.file.detector.exception;
 
+import io.github.kylinhunter.commons.exception.builtin.biz.BizException;
+
 /**
  * @author BiJi'an
  * @description custom exception for detect
  * @date 2022-10-21 00:25
  **/
-public class DetectException extends RuntimeException {
+public class DetectException extends BizException {
     public DetectException(String message) {
         super(message);
     }
